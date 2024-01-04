ac3049cbace5c2e894188d61c6b4cdb686b8d2c9
Mardhotillah1610 <155626710+Mardhotillah1610@users.noreply.github.com>
2024-01-04 16:49:05 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,30 @@
+<div class="art-post">
+<div class="art-post-body">
+<div class="art-post-inner art-article">
+<h2 class="art-postheader">About Me</h2>
+<div class="art-postcontent">
+<table width="100%" border="0" align="center">
+  <tr>
+    <td width="200" rowspan="6" bordercolor="#fddde6" bgcolor="#fddde6"><img src="woman.png" width="300" height="350" border="0"></td>
+    <td width="136" height="29"><div align="center"><span class="style7 style13">Nama</span></td>
+        <td width="2350"><span class="style7 style13">Mardhotillah</span></td>
+  </tr>
+  <tr>
+    <td height="28"><div align="center"><span class="style7 style13">NIM</span></td>
+    <td><span class="style7 style13">18010015</span></td>
+  </tr>
+  <tr>
+    <td height="29"><div align="center"><span class="style7 style13">Prodi</span></td>
+    <td><span class="style7 style13">Teknik Informatika </span></td>
+  </tr>
+  <tr>
+    <td height="28"><div align="center"><span class="style7 style13">Kampus</span></td>
+    <td><span class="style7 style13">STIKOM Muhammadiyah Batam</span></td>
+  </tr>
+</table>
+</div>
+<div class="cleared"></div>
+</div>
+    <div class="cleared"></div>
+    </div>
+</div>
